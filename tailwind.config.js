b8e083ffbffd789b9e53d74d4466a20d6b659c27
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-05 17:52:49 +0700

    build: updated tailwind class safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/index.html"],
-  safelist: ["text-slate-300/90"],
+  safelist: [
+    "text-slate-300/90",
+    "inline",
+    "rounded-full",
+    "px-2.5",
+    "py-0.5",
+    "text-xs",
+    "font-medium",
+    "bg-slate-600",
+    "text-slate-300",
+  ],
   theme: {
     fontFamily: { sans: ["Inter", "Segoe UI", "sans-serif"] },
     extend: {},
