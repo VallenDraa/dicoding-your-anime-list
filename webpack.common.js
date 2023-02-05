a2427a653b63c3fdcba2b6cc69dd92575f6a4a87
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-05 17:52:32 +0700

    build: added postcss loader

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
