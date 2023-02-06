3d1594e6e85548e20cb47466142411dc51146f1f
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-02-06 10:28:50 +0700

    build: updated webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -5,8 +5,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/scripts/index.js",
   output: {
+    publicPath: "",
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.[contenthash].js",
+    assetModuleFilename: "assets/[hash][ext]",
     clean: true,
   },
   module: {
@@ -15,12 +17,18 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.mp4$/,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "main.[contenthash].css",
-    }),
+    new MiniCssExtractPlugin({ filename: "main.[contenthash].css" }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
